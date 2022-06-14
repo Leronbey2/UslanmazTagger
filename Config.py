8da92e2af2354a198d5213f67ea2f77212de41a2
Ali930ben <102591431+Ali930ben@users.noreply.github.com>
2022-06-14 23:41:27 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -14,3 +14,5 @@ class Config():
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
     OWNER_ID = int(os.environ.get("OWNER_ID"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    
